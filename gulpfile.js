0dcd77d6890b8837b27a7f1dfd67d1ed08459e7c
htmldiz <mark.tanasevich@gmail.com>
2021-04-15 12:44:18 +0200

    update to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,177 +1,141 @@
-const autoprefixer = require('gulp-autoprefixer')
-const browserSync = require('browser-sync').create();
-const changed = require('gulp-changed')
-const cssnano = require('gulp-cssnano')
-const del = require('del')
-const gulp = require('gulp')
-const fs = require('fs')
-const gulpIf = require('gulp-if')
-const imagemin = require('gulp-imagemin')
-const imageminJpegRecompress = require('imagemin-jpeg-recompress');
-const purifycss = require('gulp-purifycss');
-const runSequence = require('gulp4-run-sequence')
-const sass = require('gulp-sass')
-const sourcemaps = require('gulp-sourcemaps')
-const uglify = require('gulp-uglify')
-const useref = require('gulp-useref')
-const rewritedep = require('./helpfilegulp/rewritedep/rewritedep').stream
-const packageFile = require('./package.json')
-const flatten = require('gulp-flatten')
-const file = require('gulp-file')
-const foreach = require("gulp-foreach");
-const devPaths = {
-  nodeFolder: 'node_modules/',
-  allCss: 'src/scss/npmdep.scss',
-  scss: 'src/scss/',
-  css: 'src/css/',
-  scripts: 'src/js/',
-  images: 'src/img/*',
-  fonts: 'src/fonts/',
-  html: 'src/',
-  footerFolder: 'src/',
-  footerTpl: 'src/*.html'
+const { watch, series, src, dest,parallel } = require('gulp');
+const browserSync  = require('browser-sync').create();
+const reload       = browserSync.reload;
+const concat       = require('gulp-concat');
+const sass         = require('gulp-sass');
+const del          = require('del');
+const gulpIf       = require('gulp-if');
+const cssnano      = require('gulp-cssnano');
+const useref       = require('gulp-useref');
+const uglify       = require('gulp-uglify')
+const imagemin       = require('gulp-imagemin')
+const config       = require('./config.js');
+const webp         = require('gulp-webp');
+var jpegRecompress = require('imagemin-jpeg-recompress');
+var cache          = require('gulp-cache')
+const list_plugins = require('./list_plugins.js');
+function webp_clean(cb) {
+    del.sync(config.basepath+config.devPaths.images + 'webp');
+    cb();
 }
-const distPaths = {
-  root: 'dist/',
-  css: 'dist/css/',
-  scripts: 'dist/js/',
-  images: 'dist/img/',
-  fonts: 'dist/fonts/',
-  html: 'dist/',
-  footerFolder: 'dist/'
+function convertImageToWebp() {
+    return src([config.basepath+config.devPaths.images + '**/*.{png,jpg,jpeg}', '!webp'])
+        .pipe(webp())
+        .pipe(dest(config.basepath+config.distPaths.images + '/webp'))
 }
-const flags = {
-  production: false
+function convertImageToWebpdevPaths() {
+    return src([config.basepath+config.devPaths.images + '**/*.{png,jpg,jpeg}', '!webp'])
+        .pipe(webp())
+        .pipe(dest(config.distPaths.images + '/webp'))
 }
-function removeDir(path) {
-  if (fs.existsSync(path)) {
-    const files = fs.readdirSync(path)
-    if (files.length > 0) {
-      files.forEach(function(filename) {
-        var remove_obj = path + "/" + filename;
-        remove_obj = remove_obj.replace(new RegExp('//','g'),'/');
-        if (!fs.statSync(remove_obj).isDirectory()) {
-          console.log(remove_obj);
-          fs.unlinkSync(remove_obj);
-        }
-      })
-    }
-    console.log("Directory removed.");
-  } else {
-    console.log("Directory path not found.");
-  }
-  return true;
+function clean(cb) {
+    del.sync(config.sync.server.baseDir);
+    del.sync(config.distPaths.headerFolder);
+    cb();
 }
-function sassChange() {
-  return gulp.src(devPaths.scss + '**/*.scss')
-    .pipe(gulpIf(!flags.production, sourcemaps.init()))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({ browsers: [
-        'last 2 versions',
-        'android 4',
-        'opera 15'] }))
-    .pipe(gulpIf(!flags.production, sourcemaps.write()))
-    // .pipe(gulpIf(flags.production, purifycss([devPaths.html + '**/*.html'])))
-    .pipe(changed(devPaths.css, { hasChanged: changed.compareSha1Digest }))
-    .pipe(gulp.dest(devPaths.css))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+function browser_sync_reload(cb) {
+    browserSync.reload();
+    cb();
 }
-
-gulp.task('npmdepStyles', function () {
-  return gulp.src(devPaths.allCss)
-    .pipe(rewritedep())
-    .pipe(gulp.dest(devPaths.scss))
-})
-
-gulp.task('npmdepScripts', function () {
-  return gulp.src(devPaths.footerTpl)
-    .pipe(rewritedep())
-    .pipe(gulp.dest(devPaths.footerFolder))
-})
-
-// Copy-paste fontawesome
-gulp.task('fonts', function() {
-  dependenciesLength = packageFile.dependencies.length;
-  dependencies = packageFile.dependencies;
-  for (var index in dependencies) {
-    gulp.src(devPaths.nodeFolder+index+'**/fonts/*.{otf,ttf,woff,woff2}')
-    .pipe(flatten())
-    .pipe(gulp.dest(devPaths.fonts))
-  }
-})
-// npmdep tasks
-gulp.task('npmdep', function(callback) {
-  runSequence('npmdepStyles', 'npmdepScripts', 'fonts',
-    callback
-  )
-})
-// Watchers
-function watchChange() {
-  browserSync.init({
-    server: {
-      baseDir: "src/",
-      routes: {"/node_modules": "node_modules"}
-    }
-  })
-  gulp.watch(devPaths.scss + '**/*.scss', sassChange)
-  gulp.watch(devPaths.scripts + '**/*.js').on('change', browserSync.reload);
-  gulp.watch(devPaths.html + '**/*.html').on('change', browserSync.reload);
-  // gulp.watch(['package.json'], ['npmdep'])
+function browser_sync_refresh(cb) {
+    browserSync.reload({stream:true});
+    cb();
 }
-
-
-// Production Tasks
-// -----------------
-//Clean before production
-// function clean_dist() {
-//   return removeDir(distPaths.root);
-// }
-// Contcatenation scripts
-function userefTask() {
-  return gulp.src(devPaths.footerTpl)
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest(distPaths.footerFolder));
+function useref_builder() {
+    return src('./'+config.devPaths.html+config.devPaths.headerTpl)
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(dest(config.distPaths.headerFolder))
 }
-// Optimizing Images
-gulp.task('images', function() {
-  return gulp.src(devPaths.images + '*')
-    .pipe(imagemin([
-      imagemin.gifsicle(),
-      imageminJpegRecompress({
-        loops:4,
-        min: 50,
-        max: 95,
-        quality:'high'
-      }),
-      imagemin.optipng()
-    ]))
-    .pipe(gulp.dest(distPaths.images))
-})
-
-gulp.task('move_css', function() {
-  return gulp.src(devPaths.css + '*.css')
-  .pipe(gulp.dest(distPaths.css))
-})
-
-//Default task - dev
-function defaultRun(callback) {
-  runSequence(['watch'],
-    callback
-  )
+function html_build(cb) {
+    src(config.basepath+config.devPaths.headerTpl)
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano({zindex: false})))
+        .pipe(dest(config.distPaths.headerFolder));
+    return cb();
 }
-gulp.task('build', function(callback) {
-  flags.production = true;
-  removeDir(distPaths.root);
-  runSequence(
-      userefTask,
-    sassChange,
-    ['images', 'fonts', "move_css"],
-    callback
-  )
-})
-exports.watch = watchChange;
-exports.default = watchChange;
+function images_build(cb) {
+    src(config.distPaths.images + '**/*.{png,jpg,jpeg,webp,svg}')
+        .pipe(dest(config.distPaths.headerFolder + config.devPaths.images));
+    return cb();
+}
+function sass_tocss() {
+    return src(config.basepath+config.devPaths.scss + '**/*.scss')
+        // .pipe(gulpIf(!flags.production, sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        // .pipe(autoprefixer({ browsers: config.settingsAutoprefixer.browsers }))
+        // .pipe(gulpIf(!flags.production, sourcemaps.write()))
+        .pipe(dest(config.basepath+config.devPaths.css))
+}
+function javascript(cb) {
+    // console.log("hello");
+    src(config.basepath+config.devPaths.scripts + '**/*.js')
+        .pipe(dest(config.distPaths.scripts));
+    cb();
+}
+function pluginsScripts(cb) {
+        src(list_plugins.list)
+        .pipe(concat('plugins.js'))
+        .pipe(dest(config.basepath+config.devPaths.scripts));
+    cb();
+}
+function css(cb) {
+    src(config.basepath+config.devPaths.css + '**/*.css')
+        .pipe(cssnano({zindex: false}))
+        .pipe(dest(config.distPaths.css))
+        .pipe(browserSync.reload({stream:true}));
+    cb();
+}
+function html_change(cb) {
+    console.log(config.basepath + '**/*.html');
+    src(config.basepath + '**/*.html')
+        .pipe(dest(config.distPaths.html));
+    cb();
+}
+function browser_sync(cb) {
+    browserSync.init(config.sync);
+    cb();
+}
+function fonts() {
+    return src(config.basepath+config.devPaths.fonts + '**/*.{woff,woff2,otf,ttf}')
+        .pipe(dest(config.distPaths.fonts))
+}
+function fonts_build() {
+    return src(config.basepath+config.devPaths.fonts + '**/*.{woff,woff2,otf,ttf}')
+        .pipe(dest(config.distPaths.headerFolder + config.devPaths.fonts))
+}
+function watch_change() {
+    watch(config.basepath+config.devPaths.scss+'**/*.scss', sass_tocss);
+    watch(config.basepath+config.devPaths.css+'**/*.css', series(css,  browser_sync_refresh));
+    watch(config.basepath+config.devPaths.scripts+'**/*.js', series(javascript,  browser_sync_reload));
+    // watch('./list_plugins.js', series(pluginsScripts,  browser_sync_reload));
+    watch(config.basepath+"**/*.html", series(html_change, browser_sync_reload ));
+    watch(config.basepath+config.devPaths.images + '**/*.{png,jpg,jpeg,svg,webp}', series(images_clean,webp_clean, convertImageToWebpdevPaths, images, images_webp ));
+    watch(config.basepath+config.devPaths.fonts+"**/*.{woff,woff2,otf,ttf}", fonts);
+    // watch(config.basepath+config.devPaths.images + '**/*.webp', images_webp);
+}
+function images_webp() {
+    return src(config.basepath+config.devPaths.images + '**/*.webp').pipe(dest(config.distPaths.images))
+}
+function images_clean(cb) {
+    // return src(config.basepath+config.distPaths.images + '**/*.webp').pipe(dest(config.distPaths.images))
+    del.sync(config.distPaths.images);
+    cb();
+}
+function images() {
+    return src(config.basepath+config.devPaths.images + '**/*.{png,jpg,jpeg,svg}')
+        .pipe(cache(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            jpegRecompress({
+                loops:4,
+                min: 50,
+                max: 95,
+                quality:'high'
+            }),
+            imagemin.optipng({optimizationLevel: 7})
+        ])))
+        .pipe(dest(config.distPaths.images))
+}
+exports.default = series(clean, webp_clean, convertImageToWebpdevPaths, images, images_webp, fonts, html_change,pluginsScripts, javascript, sass_tocss, css, browser_sync, watch_change);
+exports.build = series(clean, webp_clean, convertImageToWebp, images, images_webp, fonts, html_change,pluginsScripts, javascript, sass_tocss, css, html_build,images_build,fonts_build);
